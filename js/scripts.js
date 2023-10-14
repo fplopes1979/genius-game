3d7a1c13a1f9f78e3f8a25fd3a9fd94f284339da
fpl@cesar.school <fpl@cesar.school>
2023-10-13 22:34:02 -0300

    Jogo pronto. Homepage melhorada.

--- js/scripts.js
+++ js/scripts.js
@@ -44,6 +44,8 @@ _gui.switch.addEventListener("click", () => {
 	_data.playerSequence = [];
 
 	disablePads();
+	changePadCursor("auto");
+
 	_gui.led.classList.remove("gui__led--active");
 
 });
@@ -61,6 +63,42 @@ _gui.start.addEventListener("click", () => {
 });
 
 const padListener = (e) => {
+
+	if(!_data.playerCanPlay) return;
+
+	let soundId;
+
+	_gui.pads.forEach((pad, key) => {
+		if(pad === e.target) {
+			soundId = key;
+		}
+	});
+
+	e.target.classList.add("game__pad--active");
+
+	_data.sounds[soundId].play();
+	_data.playerSequence.push(soundId);
+
+	setTimeout(() => {
+		e.target.classList.remove("game__pad--active");
+
+		const currentMove = _data.playerSequence.length - 1;
+
+		if(_data.playerSequence[currentMove] !== _data.gameSequence[currentMove]) {
+
+			_data.playerCanPlay = false;
+			disablePads();
+			resetOrPlayAgain();
+
+		} else if(currentMove === _data.gameSequence.length -1) {
+
+			newColor();
+			playSequence();
+		}
+
+		waitForPlayerClick();
+
+	}, 250);
 
 }
 
@@ -89,6 +127,13 @@ const setScore = () => {
 
 const newColor = () => {
 
+	if(_data.score === 2) {
+		alert("Você venceu! Parabéns! Use o cupom BFDDESCONTO10 e tenha 10% de desconto em qualquer produto do site!");
+		alert("Se quiser jogar novamente, desligue e ligue o jogo e boa jogatina!");
+		_data.gameOn = false;
+		return;
+	}
+
 	_data.gameSequence.push(Math.floor(Math.random() * 4));
 	_data.score++;
 
@@ -103,16 +148,26 @@ const playSequence = () => {
 	_data.playerSequence = [];
 	_data.playerCanPlay = false;
 
+	changePadCursor("auto");
+
 	const interval = setInterval(() => {
+
+		if(!_data.gameOn) {
+			clearInterval(interval);
+			disablePads();
+			return;
+		}
 
 		if(padOn) {
 			if(counter === _data.gameSequence.length) {
-
 				clearInterval(interval);
 				disablePads();
+				waitForPlayerClick();
+				changePadCursor("pointer");
 				_data.playerCanPlay = true;
 				return;
 			}
+
 			const sndId = _data.gameSequence[counter];
 			const pad = _gui.pads[sndId];
 			_data.sounds[sndId].play();
@@ -162,10 +217,15 @@ const blink = (text, callback) => {
 
 const waitForPlayerClick = () => {
 
-	setTimeout(() => {
+	clearTimeout(_data.timeout);
+
+	_data.timeout = setTimeout(() => {
 		if(!_data.playerCanPlay) {
 			return;
 		}
+
+		disablePads();
+		resetOrPlayAgain();
 
 	}, 5000);
 
@@ -173,9 +233,32 @@ const waitForPlayerClick = () => {
 
 const resetOrPlayAgain = () => {
 
+	_data.playerCanPlay = false;
+
+	if(_data.strict) {
+		blink("!!", () => {
+
+			_data.score = 0;
+			_data.gameSequence = [];
+			startGame();
+		});
+
+	} else {
+
+		blink("!!", () => {
+			setScore();
+			playSequence();
+		});
+	}
+
 }
 
 const changePadCursor = (cursorType) => {
+
+	_gui.pads.forEach(pad => {
+		pad.style.cursor = cursorType;
+
+	});
 
 }
 
